48ae8e4b49a4d7fc15fd1d3fd08927baab2760db
akbarjane <501123445a@gmail.com>
2020-10-13 22:41:13 +0500

    3.0

--- index.js
+++ index.js
@@ -1,59 +1,121 @@
+
+
 function Eval(exp) {
-    var result;
-    var method = [
-        function (a, b) { return a * b; },
-        function (a, b) { return a / b; },
-        function (a, b) { return a + b; },
-        function (a, b) { return a - b; },
-    ];
-    var methodChar = ["*", "/", "+", "-"];
+    var result = 0;
+    var method = {
+        "/": function (Arr) {
+            var result = 1;
+            for (var i = 0; i < Arr.length; i++) {
+                result /= Number(Arr[i]);
+            }
+            return result;
+        },
+        "*": function (Arr) {
+            result = 1;
+            for (var i = 0; i < Arr.length; i++) {
+                result *= Number(Arr[i]);
+            }
+            return result;
+        },
+        "-": function (Arr) {
+            result = Number(Arr[0]);
+            for (var i = 1; i < Arr.length; i++) {
+                result -= Number(Arr[i]);
+            }
+            return result;
+        },
+        "+": function (Arr) {
+            result = 0;
+            for (var i = 0; i < Arr.length; i++) {
+                result += Number(Arr[i]);
+            }
+            return result;
+        }
+    };
+    var keys = Object.keys(method).reverse();
     exp = exp.replace(" ","");
-    var int1 = 0;
-    var int2 = 0;
-    for (var i = 0; i < exp.length; i++) {
-        var fun = method[i];
-        var part1 = exp.split(methodChar[i], 1)[0];
-        var part2 = exp.replace(part1, "");
-        part2 = part2.substr(1, part2.length - 1);
-        if (part2 !== "") {
-            int1 = Number(part1);
-            int2 = Number(part2);
-            if (!Number(part1)) {
-                result = fun(Eval(part1), int2);
-            }
-            else if (!Number(part2)) {
-                result = fun(int1, Eval(part2));
-            }
-            else {
-                result = fun(int1, int2);
+    for (var j = 0; j < keys.length; j++) {
+        var fun = method[keys[j]];
+        for (var i = 0; i < exp.length; i++) {
+            if (exp[i] === keys[j]) {
+                var tempArr = exp.split(keys[j]);
+                if (isNaN(fun(tempArr))) {
+                    var tempArr2 = new Array();
+                    for (var o = 0; o < tempArr.length; o++) {
+                        tempArr2.push(Eval2(tempArr[o]).toString());
+                    }
+                    return fun(tempArr2);
+                }
+                return fun(tempArr);
             }
         }
     }
-    return result;
+    return Number(exp);
 }
 function calc(){
     var result = "";
-    var btnArr = document.getElementsByClassName("btn");
+    var btnArr = document.getElementsByClassName("num");
     var clean = document.getElementById("clean");
     var answer = document.getElementById("answer");
-    clean.addEventListener("click",() => {
-        result = result.substring(0,result.length -1);
-        answer.innerHTML = result;
+    var equal = document.getElementById("equal");
+    var dot = document.getElementById("dot");
+    var operator = document.getElementsByClassName("oper");
+
+
+    var longPress = false;
+    var startTime = 0;
+    var endTime = 0;
+
+    equal.addEventListener("click",() => {
+        result = Eval(result.toString()).toString()
+        if(result === "Infinity"){
+            result = ""
+            answer.innerHTML = "Не правильная операция";
+        } else {
+            answer.innerHTML = result;
+        }
     })
+
+    clean.addEventListener("click",() => {
+        if(!longPress){
+            result = result.toString().substring(0,result.length -1);
+            answer.innerHTML = result;
+        } else {
+            result = "";
+            answer.innerHTML = result;
+        }
+    })
+    clean.addEventListener("mousedown",() => {
+        startTime = new Date().getTime();
+    })
+    clean.addEventListener("mouseup",() => {
+        endTime = new Date().getTime();
+        longPress = (endTime - startTime < 500) ? false : true;
+    })
+
     for (let i = 0; i < btnArr.length; i++) {
         btnArr[i].addEventListener("click",() => {
-            if(btnArr[i].innerHTML !== "C"){
-                result += btnArr[i].innerHTML;
-            }
+            var text = btnArr[i].innerHTML;          
+            result += text;
             answer.innerHTML = result;
-            if(Eval(result) != undefined && !isNaN(Eval(result)) ){
-                result = Eval(result);
-                answer.innerHTML = result;
-            }
         })
     }
+    for (let i = 0; i < operator.length; i++) {
+        operator[i].addEventListener("click",() => {
+            var text = operator[i].innerHTML;
+            var lastChar = result[result.length-1];
+            if(lastChar != text && lastChar != "."){
+                result += text;
+                answer.innerHTML = result;
+            }          
+        })
+    }
+    dot.addEventListener("click", () => {
+        var lastChar = result[result.length-1];
+        if( lastChar != "." && lastChar != "+" && lastChar != "-" && lastChar != "/" && lastChar != "*" ){
+            result += "."
+            answer.innerHTML = result;
+        }
+    })
 }
 calc()
-
-
-
